e8deb9b3e8caca5a6ae0723a0797ec09ee8e0d60
Colin Goudie <colin.goudie@tributech.com.au>
2016-05-29 18:57:10 +1000

    Raising not_found if job_id not found

--- app/controllers/progress_job/progress_controller.rb
+++ app/controllers/progress_job/progress_controller.rb
@@ -2,10 +2,16 @@ module ProgressJob
   class ProgressController < ActionController::Base
 
     def show
-      @delayed_job = Delayed::Job.find(params[:job_id])
+      @delayed_job = Delayed::Job.find_by_id(params[:job_id])
+
+      not_found if @delayed_job.nil?
+
       percentage = !@delayed_job.progress_max.zero? ? @delayed_job.progress_current / @delayed_job.progress_max.to_f * 100 : 0
       render json: @delayed_job.attributes.merge!(percentage: percentage).to_json
     end
 
+    def not_found
+      raise ActionController::RoutingError.new('Not Found')
+    end
   end
 end
